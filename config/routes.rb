20d8f66796f344425d9d5571128486e14b248ea7
Shawn Jones <bluesfansince2013@gmail.com>
2020-10-06 13:49:30 -0500

    routes created and pushed to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :users
-  resources :lists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :users, defaults:{ format: :json } do
+    post :login
+    post :create
+    delete :logout
+    get :me
+    end
+
+  namespace :lists, defaults:{ format: :json } do
+    get :index
+    get :show
+    post :create
+    patch :update
+    delete :destroy
+  end
+
+  namespace :cards, defaults:{ format: :json } do
+    get :index
+    get :show
+    post :create
+    patch :update
+    delete :destroy
+  end
 end
