a57af97e3a2804f4ed1138282548a06228a96f68
Shawn Jones <bluesfansince2013@gmail.com>
2020-10-13 13:27:50 -0500

    changes to list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
 
   # GET /lists/1
   def show
-    render json: { list: @list, card: @cards }
+    render json: @list, include: ['cards']
   end
 
   # POST /lists
